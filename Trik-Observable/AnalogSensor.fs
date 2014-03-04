bffd6de2d72fd27cd5a70c547ca5ff6538fcc2f3
Aleksandr Kirsanov <kashmervil@gmail.com>
2014-03-04 18:24:43 +0400

    snapshot

--- Trik-Observable/AnalogSensor.fs
+++ Trik-Observable/AnalogSensor.fs
@@ -4,10 +4,9 @@ open System
 open System.Reactive.Linq
 open Trik
 
-type AnalogSensor(register, rate) = 
+type AnanalogSensor(register, rate) = 
     let read _ = 
         let value = Helpers.I2C.receive register |> Helpers.limit 0 1024 // well... sensors are only 10-bit, but nevertheless ... 
         value * 100 / 1024
-    member this.Observable = Observable.Generate(read(), Helpers.konst true, read, id, Helpers.konst <| System.TimeSpan.FromMilliseconds (float rate))
-
-    
+    member val Observable = Observable.Generate(read(), Helpers.konst true, read, id, Helpers.konst <| System.TimeSpan.FromMilliseconds (float rate))
+    member x.Read() = read 0
