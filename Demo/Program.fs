a2207d08626f5e68eb1af63b9c13ff2532b85930
Aleksandr Kirsanov <kashmervil@gmail.com>
2014-03-18 17:44:47 +0400

    Simple Example

--- Demo/Program.fs
+++ Demo/Program.fs
@@ -10,30 +10,41 @@ let lpf (avg:IList<'a>->'a) (o:IObservable<_>) = o.Buffer(5).Select(avg)
 
 //let avg3 (buf:IList<_>) = 
 //        let (x',y',z') = buf.Aggregate(fun (x,y,z) (x',y', z') -> (x + x', y + y', z+ z'))
-//        x'/buf.Count, y'/buf.Count, z'/buf.Count  
-        
+//        x'/buf.Count, y'/buf.Count, z'/buf.Count 
 let log s = printfn "%s" s
+
+type Distance =  Far | Middle | Near
 
 [<EntryPoint>]
 let main _ = 
 
     log "Started"
     Helpers.I2C.init "/dev/i2c-2" 0x48 1
-    //let model =  Model.Create "config.xml"
     let model = new Model()
     log "Loaded"
-    let RightWheel = model.Motor.["JM1"]
-    let LeftWheel = model.Motor.["JM2"]
-    let wheelSpeed speed = model.AnalogSensor.["JA1"].ToObservable().Scan(fun acc x ->
-        printfn "%A" x;
-        if x > 35 then 0 else speed).DistinctUntilChanged()
 
-    let r_disp = (wheelSpeed 100).Subscribe(RightWheel)
-    let l_disp = (wheelSpeed 70).Subscribe(LeftWheel)
-    let gyro_dis = model.Gyro.ToObservable().Subscribe(fun x -> printfn "%A" x)
+    let rawToDist x = match x with 
+                      | _ when x > 60 -> Near
+                      | _ when x > 35 -> Middle
+                      | _ -> Far
+
+    let distToSpeed x = match x with
+                        | Near -> -100
+                        | Middle -> 0
+                        | Far -> 100
+
+    let rightWheel = model.Motor.["JM1"]
+    let leftWheel = model.Motor.["JM2"]
+    let frontSensor = model.AnalogSensor.["JA1"].ToObservable().Select(rawToDist)
+    let motorActions = frontSensor.Select(fun x ->  printfn "%A" x;
+                                                      distToSpeed x).DistinctUntilChanged()
+
+    let r_disp = motorActions.Subscribe(rightWheel)
+    let l_disp = motorActions.Subscribe(leftWheel)
+    //let gyro_dis = model.Gyro.ToObservable().Subscribe(fun x -> printfn "%A" x)
     log "Ready (any key to finish)"
    
-    System.Console.ReadKey() |> ignore
+    //System.Console.ReadKey() |> ignore
     //l_disp.Dispose()
     //r_disp.Dispose()
     //System.Console.ReadKey() |> ignore
