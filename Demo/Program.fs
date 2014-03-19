aeada11fb1e5551635d076a4db8f7b8622239df6
Kirill Melentyev <melentyev.k@gmail.com>
2014-03-19 19:32:18 +0400

    demo update

--- Demo/Program.fs
+++ Demo/Program.fs
@@ -10,11 +10,22 @@ type Distance =  Far | Middle | Near
 let log s = printfn s
 
 let testPad (model:Model) = 
-    //use servo1 = model.Servo.["JE2"]
-    //use servo2 = model.Servo.["JE1"]
+    use servo1 = model.Servo.["JE2"]
+    use servo2 = model.Servo.["JE1"]
+    let prX1, prY1, prX2, prY2 = ref -1, ref -1, ref -1, ref -1
     use pad = model.Pad
     use dbtn = pad.Buttons.Subscribe (fun num ->  printfn "%A" num)
-    use disp = pad.Pads.Subscribe (fun (num, coord) ->  printfn "%A %A" num coord )
+    use disp = pad.Pads.Subscribe ( fun (num, coord) ->  
+        //printfn "%A %A" num coord 
+        match num, coord with
+        | (1, Some(x, y) ) -> 
+            if !prX1 = -1 then prX1 := x; prY1 := y 
+            else servo1.SetPower(x - !prX1)
+        | (1, None) -> prX1 := -1; prY1 := -1
+        | (2, Some(x, _) ) -> servo2.SetPower(- x * 2)
+        | (2, None) -> servo2.Zero()
+        | (_, _) -> () 
+    )
 
     log "Ready (any key to finish)"
     System.Console.ReadKey() |> ignore
@@ -47,9 +58,9 @@ let main _ =
     Helpers.I2C.init "/dev/i2c-2" 0x48 1
     use model = new Model(ServoConfig = [| 
                               ("JE1", "/sys/class/pwm/ehrpwm.1:1", 
-                                { stop = 0; zero = 1310000; min = 1200000; max = 1400000; period = 20000000 } )
+                                { stop = 0; zero = 1310000; min = 1200000; max = 1420000; period = 20000000 } )
                               ("JE2", "/sys/class/pwm/ehrpwm.1:0", 
-                                { stop = 0; zero = 1550000; min = 800000; max = 2250000; period = 20000000 } )
+                                { stop = 0; zero = 1550000; min =  800000; max = 2250000; period = 20000000 } )
                              |])
     log "Loaded"
     testPad model
