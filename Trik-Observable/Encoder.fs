813a75c29e3671802c5f1f8a67c987e97ee014b9
Kirill Melentyev <melentyev.k@gmail.com>
2014-03-31 18:07:35 +0400

    Encoder

---
+++ Trik-Observable/Encoder.fs
@@ -0,0 +1,16 @@
+﻿namespace Trik
+
+open System
+open Trik
+
+type Encoder(i2cCommandNumber) as sens =
+    inherit Helpers.PollingSensor<float32>()
+    [<Literal>]
+    let parToRad = 0.03272492f
+    do sens.ReadFunc <- fun () ->
+        let data = Helpers.I2C.receive i2cCommandNumber
+        parToRad * float32 (data)
+    member x.Reset() = 
+        Helpers.I2C.send i2cCommandNumber
+    interface IDisposable with
+        member x.Dispose() = ()
