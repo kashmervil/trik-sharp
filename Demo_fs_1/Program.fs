81cc6d0f119702fa7130744ac61b55c8ea50be37
Aleksandr Kirsanov <kashmervil@gmail.com>
2014-05-12 19:42:31 +0400

    fifo button

--- Demo_fs_1/Program.fs
+++ Demo_fs_1/Program.fs
@@ -1,7 +1,20 @@
-﻿// Learn more about F# at http://fsharp.net
-// See the 'F# Tutorial' project for more help.
+﻿open System
+open Trik
+
+
+let log s = printfn s
 
 [<EntryPoint>]
-let main argv = 
-    printfn "%A" argv
-    0 // return an integer exit code
+let main _ = 
+    log "Started"
+    Helpers.I2C.init "/dev/i2c-2" 0x48 1
+    use model = new Model(ServoConfig = [| 
+                              ("JE1", "/sys/class/pwm/ehrpwm.1:1", 
+                                { stop = 0; zero = 1310000; min = 1200000; max = 1420000; period = 20000000 } )
+                              ("JE2", "/sys/class/pwm/ehrpwm.1:0", 
+                                { stop = 0; zero = 1550000; min =  800000; max = 2250000; period = 20000000 } )
+                             |])
+    log "Loaded"
+    let lt = Linetracer.Linetracer(model)
+    lt.Run()
+    0
