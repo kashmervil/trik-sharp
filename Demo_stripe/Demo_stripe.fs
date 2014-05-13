f836f3fb3e59df6a96e9566ec38c016788be9982
Aleksandr Kirsanov <kashmervil@gmail.com>
2014-05-14 02:14:40 +0400

    Demo with LedStripe and LedStripePorts

---
+++ Demo_stripe/Demo_stripe.fs
@@ -0,0 +1,11 @@
+﻿open System
+open Trik
+[<EntryPoint>]
+let main argv = 
+
+    let model = new Model()
+    model.LedStripe |> ignore
+    use disp = model.Gyro.ToObservable().Subscribe(
+                     fun (x: int array) -> model.LedStripe.SetPower(x.[0]/100, x.[1]/100, x.[2]/100))
+    Console.ReadKey() |> ignore
+    0 // return an integer exit code
