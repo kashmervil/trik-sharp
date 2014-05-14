6e43fa56238ea7fc98af2334a824910ea06249f7
Kirill Melentyev <melentyev.k@gmail.com>
2014-05-14 20:05:50 +0400

    manipulator

--- Demo_manipulator/Demo_manipulator.fs
+++ Demo_manipulator/Demo_manipulator.fs
@@ -1,6 +1,9 @@
 ﻿open System
 open System.Collections.Generic
 open Trik
+open System.Threading
+
+let exit = new EventWaitHandle(false, EventResetMode.AutoReset)
 
 [<EntryPoint>]
 let main argv = 
@@ -8,27 +11,46 @@ let main argv =
     Helpers.I2C.init "/dev/i2c-2" 0x48 1
     use model = new Model(ServoConfig = [| 
                               ("JE1", "/sys/class/pwm/ehrpwm.1:1", 
-                                { stop = 0; zero = 1310000; min = 1200000; max = 1420000; period = 20000000 } )
+                                { stop = 0; zero = 1500000; min = 800000; max = 2400000; period = 20000000 } )
                               ("JE2", "/sys/class/pwm/ehrpwm.1:0", 
-                                { stop = 0; zero = 1550000; min =  800000; max = 2250000; period = 20000000 } )
+                                { stop = 0; zero = 1500000; min = 800000; max = 2400000; period = 20000000 } )
                              |], PadConfigPort = 4444)
    
-    //let servo1 = model.Servo.["JE2"]
-    //let servo2 = model.Servo.["JE1"]
-    let prX1, prY1, prX2, prY2 = ref -1, ref -1, ref -1, ref -1
+    let servo1 = model.Servo.["JE2"]
+    let servo2 = model.Servo.["JE1"]
+    let p1 = model.Motor.["JM2"]
+    let p2 = model.Motor.["JM3"]
+    //let prX1, prY1, prX2, prY2 = ref -1, ref -1, ref -1, ref -1
     let pad = model.Pad
-    use dbtn = pad.Buttons.Subscribe (fun num ->  printfn "%A" num)
+    use dbtn = pad.Buttons.Subscribe (function 
+        | x when x = 5 -> 
+            printfn "Exiting (start)"
+            exit.Set() |> ignore
+        | num -> printfn "%A" num)
     use disp = pad.Pads.Subscribe ( fun (num, coord) ->  
         match num, coord with
         | (1, Some(x, y) ) -> 
-            printfn "servo1.SetPower(%d)" x //servo1.SetPower(x)
-        | (1, None) -> prX1 := -1; prY1 := -1; printfn "None 1";
-        | (2, Some(x, _) ) -> printfn "servo2.SetPower(- %d * 2)" x //servo2.SetPower(- x * 2)
-        | (2, None) -> printfn "None 2" //servo2.Zero();
+            printfn "servo1.SetPower(%d)" x
+            servo1.SetPower(x)
+            servo2.SetPower(- y * 2)
+        | (2, Some(x, y) ) -> 
+            printfn "power %d %d" x y
+            let base' = y
+            let add' = x / 2
+            p1.SetPower(base' + ( add') )
+            p2.SetPower(base' + (-add') )
+        (* | (1, None) -> prX1 := -1; prY1 := -1; printfn "None 1";
+        | (2, Some(x, _) ) -> 
+            printfn "servo2.SetPower(- %d * 2)" x 
+            servo2.SetPower(- x * 2) *)
+        | (2, None) -> 
+            printfn "None 2" 
+            //servo2.Zero();
         | (_, _) -> () 
     )
 
-    printfn "Ready (any key to finish)"
-    System.Console.ReadKey() |> ignore
+    printfn "Ready (5 pad btn to finish)"
+    exit.WaitOne() |> ignore
+    printfn "Exiting (after wait)"
     0
    
