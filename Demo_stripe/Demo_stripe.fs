2f9c42ccb15c997cde2096347f6ba93d8eef92eb
Aleksandr Kirsanov <kashmervil@gmail.com>
2014-05-14 18:27:25 +0400

    ledstripe init fix

--- Demo_stripe/Demo_stripe.fs
+++ Demo_stripe/Demo_stripe.fs
@@ -2,10 +2,12 @@
 open Trik
 [<EntryPoint>]
 let main argv = 
-
+    Helpers.I2C.init "/dev/i2c-2" 0x48 1
     let model = new Model()
-    model.LedStripe |> ignore
+//model.LedStripe |> ignore
+    let inline f (x:int array) = 
+        (100 - abs(x.[0]/10) - abs(x.[2]/10), abs(x.[0]/10) + abs(x.[1]/10), abs(x.[2]/10) + abs(x.[1]/10))  
     use disp = model.Gyro.ToObservable().Subscribe(
-                     fun (x: int array) -> model.LedStripe.SetPower(x.[0]/100, x.[1]/100, x.[2]/100))
+                     fun (x: int array) -> model.LedStripe.SetPower(f x))
     Console.ReadKey() |> ignore
     0 // return an integer exit code
