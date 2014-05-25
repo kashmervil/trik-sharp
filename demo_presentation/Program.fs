5a1cdfab71eaa0efc8a04a784de39d92a55fb411
Aleksandr Kirsanov <kashmervil@gmail.com>
2014-05-25 14:20:10 +0400

    presentation demo

---
+++ demo_presentation/Program.fs
@@ -0,0 +1,18 @@
+﻿open Trik
+open System.Reactive.Linq
+[<EntryPoint>]
+let main _ = 
+    use model = new Model()
+    let motorL = model.Motor.["JM2"]//Two port in controller's behind
+    let motorR = model.Motor.["JM1"]//you can change to ports you like 
+    //for full ports description and location go http://goo.gl/jRWJ4j
+    let power = model.AnalogSensor.["JA1"].ToObservable().Select(fun d -> //second port in front 
+                            if d > 55 then 100 elif d < 45 then -100 else 0).DistinctUntilChanged()
+     
+    use l_disp = power.Subscribe(motorL)
+    use r_disp = power.Subscribe(motorR)
+    use led_disp = power.Select(function | 100 -> LedColor.Green 
+                                         | 0   -> LedColor.Orange 
+                                         | _   -> LedColor.Red).Subscribe(model.Led)
+    System.Console.ReadKey() |> ignore
+    0
