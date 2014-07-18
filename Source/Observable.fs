97427e8379390c56810c62a3aae1ba7c3a1ba2c4
Aleksandr Kirsanov <kashmervil@gmail.com>
2014-07-19 03:30:02 +0400

    Introducing Threading.Timer

--- Source/Observable.fs
+++ Source/Observable.fs
@@ -6,7 +6,7 @@ module Observable =
     let Create(subscription) = { new IObservable<'T> with
                                     member self.Subscribe observer = subscription observer}
 
-    let DistinctUntilChanged (sequence: IObservable<'T>  when 'T: comparison) = 
+    let DistinctUntilChanged (sequence: IObservable<'T> when 'T: equality) = 
         let prev = ref None
         let func x = if (!prev).IsNone || (!prev).Value <> x 
                         then prev := Some x; !prev
@@ -14,33 +14,29 @@ module Observable =
         Observable.choose func sequence
     
     let Interval(timeSpan: System.TimeSpan) = 
-        let running = ref true
-        let tick = 
-            let r = ref 0
-            fun () -> r:= !r + 1; !r
-
+        let counter = ref 0
         let observers = new ResizeArray<IObserver<int>>()
+        let obsNext _ = lock (observers) <|
+                        fun () -> 
+                            observers |> Seq.iter (fun obs -> obs.OnNext(!counter)) 
+                            lock (counter) <|
+                                fun () -> incr counter
+
+        let timer = new Threading.Timer(obsNext, null, TimeSpan.Zero, timeSpan)
+
         let observable = {
             new IObservable<int> with
                 member self.Subscribe observer = 
                     lock observers <| fun () -> observers.Add(observer)
                     { new IDisposable with 
-                        member self.Dispose() = lock observers <| fun () -> observers.Remove(observer) |> ignore 
+                        member self.Dispose() = lock (observers) <|
+                                                    fun () -> observers.Remove(observer) 
+                                                    |> ignore 
                     }
             interface IDisposable with
                 member self.Dispose() = 
-                    running := false
-                    observers.RemoveAll(fun _ -> true) |> ignore
-                    ()
+                    timer.Dispose()
+                    observers.Clear()
             }
-
-        let obsNext x = lock observers <| fun () -> observers |> Seq.iter (fun obs -> obs.OnNext(x)) 
-        let rec triggering() = async {
-                             if !running then
-                                 obsNext(tick())
-                                 System.Threading.Thread.Sleep(int timeSpan.Ticks/10900)
-                                 return! triggering()
-                             }
-        triggering() |> Async.Start
         observable
 
