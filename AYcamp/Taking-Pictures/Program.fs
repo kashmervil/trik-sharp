04a24c864e73d3679c46609df6d096f992f55717
Aleksandr Kirsanov <kashmervil@gmail.com>
2014-08-18 21:39:01 +0400

    taking picture example added

---
+++ AYcamp/Taking-Pictures/Program.fs
@@ -0,0 +1,15 @@
+﻿open Trik.Junior
+open Trik.Collections
+
+let mutable button = ButtonEventCode.Down
+robot.ButtonPad.Start()
+printfn "Press any key to start"
+robot.ButtonPad.Read() |> ignore
+
+while button <> ButtonEventCode.Menu do
+    printfn "Taking a picture"
+    robot.TakePicture()
+    printfn "Press any key to continue or Menu to exit\n"
+    button <- robot.ButtonPad.Read().Button
+    
+    
