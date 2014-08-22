a039696c87b1cca8f93cc08ba0507e826d96d7ed
Aleksandr Kirsanov <kashmervil@gmail.com>
2014-08-22 12:14:20 +0400

    Encoder Change

--- Source/Encoder.fs
+++ Source/Encoder.fs
@@ -4,6 +4,6 @@ open Trik
 type Encoder(i2cCommandNumber) =
     inherit Helpers.PollingSensor<int>()
     override self.Read() = Helpers.I2C.Receive i2cCommandNumber
-    member self.Reset() = Helpers.I2C.Send i2cCommandNumber
+    member self.Reset() = Helpers.I2C.Send i2cCommandNumber 0 1
     interface IDisposable with
         member x.Dispose() = ()
--- Source/Junior.fs
+++ Source/Junior.fs
@@ -28,6 +28,7 @@ type Robot() as is =
 
     member self.Led = super.Led
 
+    member self.Encoder = super.Encoder.Values |> Seq.mapi (fun i x -> Ports.Encoder.Values.[i], x) |> dict
     member self.Motor = super.Motor.Values |> Seq.mapi (fun i x -> Ports.Motor.Values.[i], x) |> dict
     member self.Sensor = super.AnalogSensor.Values |> Seq.mapi (fun i x -> Ports.Sensor.Values.[i], x) |> dict
     member self.Servo = super.Servo.Values |> Seq.mapi (fun i x -> Ports.Servo.Values.[i], x) |> dict
@@ -39,7 +40,6 @@ type Robot() as is =
     member self.Stop() = self.Motor.Values |> Seq.iter (fun x -> x.Stop())
                          self.Servo.Values |> Seq.iter (fun x -> x.Zero()) 
     
-    member self.Encoder = super.Encoder
     member self.LineSensor = super.LineSensor
     member self.Sleep(sec: float) = System.Threading.Thread.Sleep(int <| sec * 1000.)
     member self.Sleep(millisec: int) = System.Threading.Thread.Sleep(millisec)
