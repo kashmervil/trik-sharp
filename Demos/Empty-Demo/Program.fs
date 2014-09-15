e3bccb883f1423494236ab6656f9205c3fc64b6a
Aleksandr Kirsanov <kashmervil@gmail.com>
2014-09-15 20:01:48 +0400

    Post-Build events edited. Empty-Demo added

---
+++ Demos/Empty-Demo/Program.fs
@@ -0,0 +1,10 @@
+﻿open Trik
+open System.Threading
+
+let led = new Led("/sys/class/leds/")
+
+for color in let cs = [LedColor.Green; LedColor.Orange; LedColor.Red] in cs @ cs @ cs do
+    led.SetColor color
+    Thread.Sleep 300
+
+led.SetColor LedColor.Off
