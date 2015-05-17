ef5416cbb527e4b6f1aa18520a798af11d486f7a
Roman Belkov <romanbelkov@gmail.com>
2015-05-17 15:45:42 +0300

    Hunter-Demo introduced

---
+++ Demos/Hunter-Demo/C#/Constants.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TRIK_Hunter
+{
+    class Constants
+    {
+        public const int    ServoBorder = 90;
+        public const int    MaxAngleX   = 60;
+        public const int    MaxAngleY   = 60;
+        public const int    ScaleConstX = 20;
+        public const int    ScaleConstY = 20;
+        public const double RGBdepth    = 100.0;
+        public const int    MinMass     = 5;
+    }
+
+}
